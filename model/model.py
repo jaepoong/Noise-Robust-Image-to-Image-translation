600289d5f95b68d0684ecc79cdeff80b167358cc
jaepoong <tjfwownd@naver.com>
2021-11-21 21:45:50 +0900

    네트워크 완료

--- model/model.py
+++ model/model.py
@@ -23,7 +23,28 @@ class Generator(nn.Module):
         c=c.repeat(1,1,x.size(2),x.size(3))
         x=torch.cat([x,c],dim=1)
         return self.layer(x)
-    
+
+class NoiseGenerator(nn.Module):
+
+        def __init__(self):
+            super(self,NoiseGenerator).__init__()
+            layers=[]
+            layers.append(Down_Sampling(3,64,kernel_size=7,stride=1,padding=3))
+            layers.append(Down_Sampling(64,128,kernel_size=4,stride=2,padding=1))
+            layers.append(Down_Sampling(128,256,kernel_size=4,stride=2,padding=1))
+            for i in range(repeat_num):
+                layers.append(ResidualBlock(256,256))
+        
+            layers.append(Up_Sampling(256,128,kernel_size=4,stirde=2,padding=1))
+            layers.append(Up_Sampling(128,64,kernel_size=4,stride=2,padding=1))
+            layers.append(nn.Conv2d(64,3,kernel_size=7,stride=1,padding=3))
+        
+            layer=nn.Sequential(*layers)
+            
+        def forward(self,x):
+            return self.layer(x)
+                
+            
 
 class Discriminator(nn.Module):
     """ patchGan """
@@ -40,7 +61,7 @@ class Discriminator(nn.Module):
         
         self.conv=nn.Sequential(*layers)
         self.conv1=nn.Conv2d(dim,1,kernel_size=3,stride=-1,padding=1)# Real/False 판별기
-        self.conv2=nn.Conv2d(dim,condition,kernel_size=kernel_size)
+        self.conv2=nn.Conv2d(dim,condition,kernel_size=kernel_size) # class 판별기
         
     def forward(self,x):
         x=self.conv(x)
--- model/sub.py
+++ model/sub.py
@@ -17,6 +17,7 @@ class ResidualBlock(nn.Module):
     def forward(self,x):
         return x+self.conv(x)
 
+
 class Down_Sampling(nn.Module):
     
     def __init__(self,in_channels,out_channels,kernel_size,stride,padding):
