da15d30e1421e044b62c2d68b2962e8de483c2aa
jaepoong <tjfwownd@naver.com>
2021-11-22 21:48:47 +0900

    dtd

--- model/model.py
+++ model/model.py
@@ -3,9 +3,10 @@ from sub import *
 
 class Generator(nn.Module):
     
-    def __init__(self,condition=5,repeat_num=6):
+    def __init__(self,repeat_num=6):
+        super(self,Generator).__init__()
         layers=[]
-        layers.append(Down_Sampling(3+condition,64,kernel_size=7,stride=1,padding=3))
+        layers.append(Down_Sampling(3,64,kernel_size=7,stride=1,padding=3))
         layers.append(Down_Sampling(64,128,kernel_size=4,sride=2,padding=1))
         layers.append(Down_Sampling(128,256,kernel_size=4,stride=2,padding=1))
         
@@ -18,32 +19,8 @@ class Generator(nn.Module):
         
         layer=nn.Sequential(*layers)
     
-    def forward(self,x,c):    
-        c=c.view(c.size(0),c.size(1),1,1)
-        c=c.repeat(1,1,x.size(2),x.size(3))
-        x=torch.cat([x,c],dim=1)
+    def forward(self,x):    
         return self.layer(x)
-
-class NoiseGenerator(nn.Module):
-
-        def __init__(self):
-            super(self,NoiseGenerator).__init__()
-            layers=[]
-            layers.append(Down_Sampling(3,64,kernel_size=7,stride=1,padding=3))
-            layers.append(Down_Sampling(64,128,kernel_size=4,stride=2,padding=1))
-            layers.append(Down_Sampling(128,256,kernel_size=4,stride=2,padding=1))
-            for i in range(repeat_num):
-                layers.append(ResidualBlock(256,256))
-        
-            layers.append(Up_Sampling(256,128,kernel_size=4,stirde=2,padding=1))
-            layers.append(Up_Sampling(128,64,kernel_size=4,stride=2,padding=1))
-            layers.append(nn.Conv2d(64,3,kernel_size=7,stride=1,padding=3))
-        
-            layer=nn.Sequential(*layers)
-            
-        def forward(self,x):
-            return self.layer(x)
-                
             
 
 class Discriminator(nn.Module):
@@ -54,7 +31,7 @@ class Discriminator(nn.Module):
         layers.append(nn.LeakyReLU(0.2))
 
         for i in range(1,repeat_num):
-            layers.append(nn.Conv2d(dim,dim*2,kernel_size=4,stride=2,padding1))
+            layers.append(nn.Conv2d(dim,dim*2,kernel_size=4,stride=2,padding=1))
             layers.append(nn.LeakyReLU(0.2))
             dim=dim*2
         kernel_size=int(image_size/np.power(2,repeat_num))
@@ -68,6 +45,3 @@ class Discriminator(nn.Module):
         src=self.conv1(x)
         cls=self.conv2(x)
         return src,cls.view(cls.size(0),cls.size(1))
-        
-
-        
